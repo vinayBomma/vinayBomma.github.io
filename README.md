1fdba85ceaa8511610504163d53ffc7b1ada7bb2
vinayBomma <vinaysb2000@gmail.com>
2021-06-21 19:34:31 +0530

    portfolio init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio
+
+A simple portfolio starter theme for Gridsome
+
+## Website URL
+
+[View Portfolio](https://vinayBomma.github.io)
---
+++ gridsome.config.js
@@ -0,0 +1,82 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Vinay Bomma',
+  siteDescription: 'A simple portfolio made in Vuejs',
+  siteUrl: 'https://vinayBomma.github.io',
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+    },
+    {
+      use: '@gridsome/vue-remark',
+      options: {
+        typeName: 'Documentation', // Required
+        baseDir: './docs', // Where .md files are located
+        pathPrefix: '/docs', // Add route prefix. Optional
+        template: './src/templates/Documentation.vue', // Optional
+        plugins: [
+          [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+      ],
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Gridsome Portfolio Starter Blog',
+          // feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
+          // site_url: 'https://gridsome-portfolio-starter.netlify.com/'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.summary,
+          url: 'https://vinayBomma.github.io' + node.path,
+          author: 'Vinay Bomma',
+          date: node.date
+        }),
+        output: {
+          dir: './static',
+          name: 'rss.xml'
+        }
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      }
+    },
+  ],
+  templates: {
+    Tag: '/tag/:id'
+  },
+  transformers: {
+    remark: {
+      plugins: [
+        [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+      ],
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+    }
+  },
+}
